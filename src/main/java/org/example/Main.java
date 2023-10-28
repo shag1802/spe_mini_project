5f1c6cfc70396d08d5ef7ff7e57e4afd45d75b18
shag1802 <agrawalshaurya2002@gmail.com>
2023-10-28 16:32:17 +0530

    Trying to add log func

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,10 +2,15 @@ package org.example;
 
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 public class Main {
+
+    private static final Logger logger =LogManager.getLogger(Main.class);
     public static void main(String[] args) {
         // Press Alt+Enter with your caret at the highlighted text to see how
         // IntelliJ IDEA suggests fixing it.
+        logger.info("Start of Execution");
         System.out.printf("Hello and welcome!");
 
         // Press Shift+F10 or click the green arrow button in the gutter to run the code.
