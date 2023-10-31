e85398e358b4164236501b9ea6716de0b8c16042
shag1802 <agrawalshaurya2002@gmail.com>
2023-10-31 16:11:56 +0530

    some changes

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -29,7 +29,7 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 import java.util.Scanner;
-
+//
 public class Main {
     private static final Logger logger = LogManager.getLogger(Main.class);
     public static void main(String[] args) {
