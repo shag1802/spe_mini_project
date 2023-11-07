f49518a0e3e4a916e534c9cc329d60e5885792d9
shag1802 <agrawalshaurya2002@gmail.com>
2023-11-07 15:33:06 +0530

    Removing some comments

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,29 +1,3 @@
-//package org.example;
-//
-//// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-//// then press Enter. You can now see whitespace characters in your code.
-//import org.apache.logging.log4j.LogManager;
-//import org.apache.logging.log4j.Logger;
-//public class Main {
-//
-//    private static final Logger logger =LogManager.getLogger(Main.class);
-//    public static void main(String[] args) {
-//        // Press Alt+Enter with your caret at the highlighted text to see how
-//        // IntelliJ IDEA suggests fixing it.
-////        This is just a comment done for testing git
-//
-//        logger.info("Start of Execution");
-//        System.out.printf("Hello and welcome!");
-//
-//        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-//        for (int i = 1; i <= 5; i++) {
-//
-//            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-//            // for you, but you can always add more by pressing Ctrl+F8.
-//            System.out.println("i = " + i);
-//        }
-//    }
-//}
 package org.example;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
